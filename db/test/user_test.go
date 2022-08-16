4095c57685fd338d316b471b457e487fdafeb1f6
dongocanh96 <ngocanh6111996@gmail.com>
2022-08-17 00:19:29 +0700

    change sql query for user and implement unit test for user

---
+++ db/test/user_test.go
@@ -0,0 +1,334 @@
+package db
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	db "github.com/dongocanh96/class_manager_go/db/sqlc"
+	"github.com/dongocanh96/class_manager_go/util"
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomUser(t *testing.T) db.User {
+	hashPassword, err := util.HashPassword(util.RandomString(6))
+	require.NoError(t, err)
+
+	arg := db.CreateUserParams{
+		Username:       util.RandomString(6),
+		HashedPassword: hashPassword,
+		Fullname:       util.RandomString(6),
+		Email:          util.RandomEmail(),
+		PhoneNumber:    util.RandomPhoneNumber(),
+		IsTeacher:      util.RandomBoolean(),
+	}
+
+	user, err := testQueries.CreateUser(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, arg.Username, user.Username)
+	require.Equal(t, arg.HashedPassword, user.HashedPassword)
+	require.Equal(t, arg.Fullname, user.Fullname)
+	require.Equal(t, arg.Email, user.Email)
+	require.Equal(t, arg.PhoneNumber, user.PhoneNumber)
+	require.Equal(t, arg.IsTeacher, user.IsTeacher)
+	require.NotZero(t, user.CreatedAt)
+	require.True(t, user.PasswordChangedAt.IsZero())
+
+	return user
+}
+
+func createRandomTeacher(t *testing.T) db.User {
+	hashPassword, err := util.HashPassword(util.RandomString(6))
+	require.NoError(t, err)
+
+	arg := db.CreateUserParams{
+		Username:       util.RandomString(6),
+		HashedPassword: hashPassword,
+		Fullname:       util.RandomString(6),
+		Email:          util.RandomEmail(),
+		PhoneNumber:    util.RandomPhoneNumber(),
+		IsTeacher:      true,
+	}
+
+	user, err := testQueries.CreateUser(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, arg.Username, user.Username)
+	require.Equal(t, arg.HashedPassword, user.HashedPassword)
+	require.Equal(t, arg.Fullname, user.Fullname)
+	require.Equal(t, arg.Email, user.Email)
+	require.Equal(t, arg.PhoneNumber, user.PhoneNumber)
+	require.Equal(t, arg.IsTeacher, user.IsTeacher)
+	require.NotZero(t, user.CreatedAt)
+	require.True(t, user.PasswordChangedAt.IsZero())
+
+	return user
+}
+
+func createRandomStudent(t *testing.T) db.User {
+	hashPassword, err := util.HashPassword(util.RandomString(6))
+	require.NoError(t, err)
+
+	arg := db.CreateUserParams{
+		Username:       util.RandomString(6),
+		HashedPassword: hashPassword,
+		Fullname:       util.RandomString(6),
+		Email:          util.RandomEmail(),
+		PhoneNumber:    util.RandomPhoneNumber(),
+		IsTeacher:      false,
+	}
+
+	user, err := testQueries.CreateUser(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, arg.Username, user.Username)
+	require.Equal(t, arg.HashedPassword, user.HashedPassword)
+	require.Equal(t, arg.Fullname, user.Fullname)
+	require.Equal(t, arg.Email, user.Email)
+	require.Equal(t, arg.PhoneNumber, user.PhoneNumber)
+	require.Equal(t, arg.IsTeacher, user.IsTeacher)
+	require.NotZero(t, user.CreatedAt)
+	require.True(t, user.PasswordChangedAt.IsZero())
+
+	return user
+}
+
+func TestCreateUser(t *testing.T) {
+	user := createRandomUser(t)
+	testQueries.DeleteUser(context.Background(), user.ID)
+}
+
+func TestGetUser(t *testing.T) {
+	user1 := createRandomUser(t)
+	user2, err := testQueries.GetUser(context.Background(), user1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+
+	require.Equal(t, user1.ID, user2.ID)
+	require.Equal(t, user1.Username, user2.Username)
+	require.Equal(t, user1.HashedPassword, user2.HashedPassword)
+	require.Equal(t, user1.Fullname, user2.Fullname)
+	require.Equal(t, user1.Email, user2.Email)
+	require.Equal(t, user1.PhoneNumber, user2.PhoneNumber)
+	require.Equal(t, user1.IsTeacher, user2.IsTeacher)
+	require.WithinDuration(t, user1.PasswordChangedAt, user2.PasswordChangedAt, time.Second)
+	require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+
+	testQueries.DeleteUser(context.Background(), user1.ID)
+}
+
+func TestDeleteUser(t *testing.T) {
+	user := createRandomUser(t)
+	err := testQueries.DeleteUser(context.Background(), user.ID)
+	require.NoError(t, err)
+}
+
+func TestListUser(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomUser(t)
+	}
+
+	arg := db.ListUsersParams{
+		Limit:  10,
+		Offset: 0,
+	}
+
+	users, err := testQueries.ListUsers(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, users)
+
+	for _, user := range users {
+		require.NotEmpty(t, user)
+	}
+
+	for i := range users {
+		testQueries.DeleteUser(context.Background(), users[i].ID)
+	}
+}
+
+func TestListTeacher(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomTeacher(t)
+	}
+
+	arg := db.ListTeachersOrStudentsParams{
+		IsTeacher: true,
+		Limit:     10,
+		Offset:    0,
+	}
+
+	users, err := testQueries.ListTeachersOrStudents(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, users)
+
+	for _, user := range users {
+		require.NotEmpty(t, user)
+	}
+
+	for i := range users {
+		testQueries.DeleteUser(context.Background(), users[i].ID)
+	}
+}
+
+func TestListStudent(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		createRandomStudent(t)
+	}
+
+	arg := db.ListTeachersOrStudentsParams{
+		IsTeacher: false,
+		Limit:     10,
+		Offset:    0,
+	}
+
+	users, err := testQueries.ListTeachersOrStudents(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, users)
+
+	for _, user := range users {
+		require.NotEmpty(t, user)
+	}
+
+	for i := range users {
+		testQueries.DeleteUser(context.Background(), users[i].ID)
+	}
+}
+
+func TestUpdateUsername(t *testing.T) {
+	user1 := createRandomUser(t)
+
+	arg := db.UpdateUsernameParams{
+		ID:       user1.ID,
+		Username: "ngoc anh",
+	}
+
+	user2, err := testQueries.UpdateUsername(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+
+	require.Equal(t, arg.Username, user2.Username)
+
+	require.Equal(t, user1.ID, user2.ID)
+	require.Equal(t, user1.HashedPassword, user2.HashedPassword)
+	require.Equal(t, user1.Fullname, user2.Fullname)
+	require.Equal(t, user1.Email, user2.Email)
+	require.Equal(t, user1.PhoneNumber, user2.PhoneNumber)
+	require.Equal(t, user1.IsTeacher, user2.IsTeacher)
+	require.WithinDuration(t, user1.PasswordChangedAt, user2.PasswordChangedAt, time.Second)
+	require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+
+	testQueries.DeleteUser(context.Background(), user1.ID)
+}
+
+func TestUpdateHashedPassword(t *testing.T) {
+	user1 := createRandomUser(t)
+	newHashedPassword, err := util.HashPassword(util.RandomString(6))
+	require.NoError(t, err)
+
+	arg := db.UpdateHashedPasswordParams{
+		ID:                user1.ID,
+		HashedPassword:    newHashedPassword,
+		PasswordChangedAt: time.Now(),
+	}
+	user2, err := testQueries.UpdateHashedPassword(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+
+	require.Equal(t, arg.HashedPassword, user2.HashedPassword)
+	require.WithinDuration(t, arg.PasswordChangedAt, user2.PasswordChangedAt, time.Second)
+
+	require.Equal(t, user1.ID, user2.ID)
+	require.Equal(t, user1.Username, user2.Username)
+	require.Equal(t, user1.Fullname, user2.Fullname)
+	require.Equal(t, user1.Email, user2.Email)
+	require.Equal(t, user1.PhoneNumber, user2.PhoneNumber)
+	require.Equal(t, user1.IsTeacher, user2.IsTeacher)
+	require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+
+	testQueries.DeleteUser(context.Background(), user1.ID)
+}
+
+func TestUpdateFullName(t *testing.T) {
+	user1 := createRandomUser(t)
+	fullname := util.RandomString(6)
+
+	arg := db.UpdateFullnameParams{
+		ID:       user1.ID,
+		Fullname: fullname,
+	}
+
+	user2, err := testQueries.UpdateFullname(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+
+	require.Equal(t, arg.Fullname, user2.Fullname)
+
+	require.Equal(t, user1.ID, user2.ID)
+	require.Equal(t, user1.Username, user2.Username)
+	require.Equal(t, user1.HashedPassword, user2.HashedPassword)
+	require.Equal(t, user1.Email, user2.Email)
+	require.Equal(t, user1.PhoneNumber, user2.PhoneNumber)
+	require.Equal(t, user1.IsTeacher, user2.IsTeacher)
+	require.WithinDuration(t, user1.PasswordChangedAt, user2.PasswordChangedAt, time.Second)
+	require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+
+	testQueries.DeleteUser(context.Background(), user1.ID)
+}
+
+func TestUpdateEmail(t *testing.T) {
+	user1 := createRandomUser(t)
+	email := util.RandomEmail()
+
+	arg := db.UpdateEmailParams{
+		ID:    user1.ID,
+		Email: email,
+	}
+
+	user2, err := testQueries.UpdateEmail(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+
+	require.Equal(t, arg.Email, user2.Email)
+
+	require.Equal(t, user1.ID, user2.ID)
+	require.Equal(t, user1.Username, user2.Username)
+	require.Equal(t, user1.HashedPassword, user2.HashedPassword)
+	require.Equal(t, user1.Fullname, user2.Fullname)
+	require.Equal(t, user1.PhoneNumber, user2.PhoneNumber)
+	require.Equal(t, user1.IsTeacher, user2.IsTeacher)
+	require.WithinDuration(t, user1.PasswordChangedAt, user2.PasswordChangedAt, time.Second)
+	require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+
+	testQueries.DeleteUser(context.Background(), user1.ID)
+}
+
+func TestUpdatePhoneNumber(t *testing.T) {
+	user1 := createRandomUser(t)
+	phoneNumber := util.RandomPhoneNumber()
+
+	arg := db.UpdatePhoneNumberParams{
+		ID:          user1.ID,
+		PhoneNumber: phoneNumber,
+	}
+
+	user2, err := testQueries.UpdatePhoneNumber(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, user2)
+
+	require.Equal(t, arg.PhoneNumber, user2.PhoneNumber)
+
+	require.Equal(t, user1.ID, user2.ID)
+	require.Equal(t, user1.Username, user2.Username)
+	require.Equal(t, user1.HashedPassword, user2.HashedPassword)
+	require.Equal(t, user1.Fullname, user2.Fullname)
+	require.Equal(t, user1.Email, user2.Email)
+	require.Equal(t, user1.IsTeacher, user2.IsTeacher)
+	require.WithinDuration(t, user1.PasswordChangedAt, user2.PasswordChangedAt, time.Second)
+	require.WithinDuration(t, user1.CreatedAt, user2.CreatedAt, time.Second)
+
+	testQueries.DeleteUser(context.Background(), user1.ID)
+}
---
+++ util/random.go
@@ -0,0 +1,53 @@
+package util
+
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const (
+	alphabet string = "abcdefghijklmnopqrstuvwxyz"
+	digit    string = "1234567"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+func RandomPhoneNumber() string {
+	var sb strings.Builder
+	k := len(digit)
+
+	for i := 0; i < 10; i++ {
+		c := digit[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+func RandomEmail() string {
+	return fmt.Sprintf("%s@email.com", RandomString(6))
+}
+
+func RandomBoolean() bool {
+	return rand.Float32() < 0.5
+}
